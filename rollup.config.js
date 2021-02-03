474522dc6ccaab37b7c759866a2c618132cdc87a
ᑭᖇᗴᔕᕼ ᗝᑎƳᗴᗴ <presh_onyee@outlook.com>
2021-02-03 14:41:51 -0800

    release: v1.0.0-beta.7

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,28 @@
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
 
-const packageJson = require("./package.json");
+const packageJson = require('./package.json');
 
 export default {
-  input: "src/index.ts",
+  input: 'src/index.ts',
   output: [
     {
       file: packageJson.main,
-      format: "cjs",
-      sourcemap: true
+      format: 'cjs',
+      sourcemap: true,
     },
     {
       file: packageJson.module,
-      format: "esm",
-      sourcemap: true
-    }
+      format: 'esm',
+      sourcemap: true,
+    },
   ],
   plugins: [
     peerDepsExternal(),
     resolve(),
     commonjs(),
-    typescript({ useTsconfigDeclarationDir: true })
-  ]
+    typescript({ useTsconfigDeclarationDir: true }),
+  ],
 };
