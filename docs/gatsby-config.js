291ec3c6795ebf1d23f85d60ce06cb1062683c81
ᑭᖇᗴᔕᕼ ᗝᑎƳᗴᗴ <presh_onyee@outlook.com>
2021-03-27 03:19:23 -0700

    feat: add new standalone documentation page

---
+++ docs/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: "smooth-doc",
+      options: {
+        name: "Camara Design System",
+        description:
+          "UI foundations for accessible, beautiful, and responsive interfaces.",
+        siteUrl: "https://camara.space",
+        author: "Presh Onyee",
+        sections: ["Components"],
+        navItems: [{ title: "Docs", url: "/docs/" }],
+        twitterAccount: "preshonyee",
+        githubRepositoryURL: "https://github.com/camaradotspace/camara-react",
+      },
+    },
+  ],
+};
