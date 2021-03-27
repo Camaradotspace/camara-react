c7bfe1555d61508109d8fe69251d9a7a9643cea2
ᑭᖇᗴᔕᕼ ᗝᑎƳᗴᗴ <presh_onyee@outlook.com>
2021-03-27 04:45:42 -0700

    fix(button): update stories

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -5,4 +5,64 @@ export default {
   title: 'Button',
 };
 
-export const Primary = () => <Button>Button</Button>;
+// variants
+export const Primary = () => <Button variant='primary'>Primary</Button>;
+
+export const Secondary = () => <Button variant='secondary'> Secondary</Button>;
+
+export const Ghost = () => <Button variant='ghost'>Ghost</Button>;
+
+// sizes
+
+export const Large = () => (
+  <Button variant='primary' size='large'>
+    Large
+  </Button>
+);
+
+export const Medium = () => (
+  <Button variant='primary' size='medium'>
+    Medium
+  </Button>
+);
+
+export const Small = () => (
+  <Button variant='primary' size='small'>
+    Small
+  </Button>
+);
+
+// disabled button
+export const Disabled = () => (
+  <Button variant='primary' disabled>
+    Disabled
+  </Button>
+);
+
+// danger button
+export const Danger = () => (
+  <Button variant='primary' danger>
+    Danger
+  </Button>
+);
+
+// pill-shaped button
+export const Pill = () => (
+  <Button variant='primary' pill>
+    Camara
+  </Button>
+);
+
+// With background color
+export const BackgroundColor = () => (
+  <Button variant='primary' pill backgroundColor='green'>
+    Camara
+  </Button>
+);
+
+// With color
+export const Color = () => (
+  <Button variant='primary' pill backgroundColor='#ffc108' color='#000000'>
+    Camara
+  </Button>
+);
