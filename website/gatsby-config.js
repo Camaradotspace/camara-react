ed72450280d9210a768ab12c0ee38652f80e4575
ᑭᖇᗴᔕᕼ ᗝᑎƳᗴᗴ <presh_onyee@outlook.com>
2021-04-05 07:57:21 -0700

    docs: add new set of docs
    add guide, oss, resources, releases, docs

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -1,17 +1,17 @@
 module.exports = {
   plugins: [
     {
-      resolve: "smooth-doc",
+      resolve: 'smooth-doc',
       options: {
-        name: "Camara Design System",
+        name: 'Camara Design System',
         description:
-          "UI foundations for accessible, beautiful, and responsive interfaces.",
-        siteUrl: "https://camara.space",
-        author: "Presh Onyee",
-        sections: ["Components"],
-        navItems: [{ title: "Docs", url: "/docs/" }],
-        twitterAccount: "preshonyee",
-        githubRepositoryURL: "https://github.com/camaradotspace/camara-react",
+          'UI foundations for accessible, beautiful, and responsive interfaces.',
+        siteUrl: 'https://camara.space',
+        author: 'Presh Onyee',
+        sections: ['Components'],
+        navItems: [{ title: 'Docs', url: '/docs/' }],
+        twitterAccount: 'preshonyee',
+        githubRepositoryURL: 'https://github.com/camaradotspace/camara-react',
       },
     },
   ],
