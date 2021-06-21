d18e4fdbc6cc62d05448d5aab6188955172e6072
ᑭᖇᗴᔕᕼ ᗝᑎƳᗴᗴ <presh_onyee@outlook.com>
2021-06-21 13:03:20 +0100

    docs(website): fix cta buttons

--- website/components/HomePage.jsx
+++ website/components/HomePage.jsx
@@ -117,59 +117,59 @@ const Wrapper = styled.div`
 const HomePage = () => {
   return (
     <Wrapper>
-      <div className='body'>
+      <div className="body">
         <div>
           <Helmet>
             {/* Primary Meta Tags */}
             <title>Camara Design System</title>
-            <link rel='preconnect' href='https://fonts.gstatic.com' />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link
-              href='https://fonts.googleapis.com/css2?family=DM+Mono:wght@300;500&display=swap'
-              rel='stylesheet'
+              href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@300;500&display=swap"
+              rel="stylesheet"
             />
-            <meta name='title' content='Camara Design System' />
+            <meta name="title" content="Camara Design System" />
             <meta
-              name='description'
-              content='UI foundations for building accessible, beautiful, and responsive web and mobile interfaces - with support for React and React Native.'
+              name="description"
+              content="UI foundations for building accessible, beautiful, and responsive web and mobile interfaces - with support for React and React Native."
             />
 
             {/* Open Graph / Facebook */}
-            <meta property='og:type' content='website' />
-            <meta property='og:url' content='https://camara.space/' />
-            <meta property='og:title' content='Camara Design System' />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://camara.space/" />
+            <meta property="og:title" content="Camara Design System" />
             <meta
-              property='og:description'
-              content='UI foundations for building accessible, beautiful, and responsive web and mobile interfaces - with support for React and React Native.'
+              property="og:description"
+              content="UI foundations for building accessible, beautiful, and responsive web and mobile interfaces - with support for React and React Native."
             />
             <meta
-              property='og:image'
-              content='https://res.cloudinary.com/beaniegram/image/upload/v1620642927/Personal%20Site/camara-design-system.png'
+              property="og:image"
+              content="https://res.cloudinary.com/beaniegram/image/upload/v1620642927/Personal%20Site/camara-design-system.png"
             />
 
             {/* Twitter */}
-            <meta property='twitter:card' content='summary_large_image' />
-            <meta property='twitter:url' content='https://camara.space/' />
-            <meta property='twitter:title' content='Camara Design System' />
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:url" content="https://camara.space/" />
+            <meta property="twitter:title" content="Camara Design System" />
             <meta
-              property='twitter:description'
-              content='UI foundations for building accessible, beautiful, and responsive web and mobile interfaces - with support for React and React Native.'
+              property="twitter:description"
+              content="UI foundations for building accessible, beautiful, and responsive web and mobile interfaces - with support for React and React Native."
             />
             <meta
-              property='twitter:image'
-              content='https://res.cloudinary.com/beaniegram/image/upload/v1620642927/Personal%20Site/camara-design-system.png'
+              property="twitter:image"
+              content="https://res.cloudinary.com/beaniegram/image/upload/v1620642927/Personal%20Site/camara-design-system.png"
             />
             <script
               async
               defer
-              data-domain='camara.space'
-              src='https://plausible.io/js/plausible.js'
+              data-domain="camara.space"
+              src="https://plausible.io/js/plausible.js"
             ></script>
           </Helmet>
           {/* Logo */}
           {/* <img src='../images/logo-nav-dark.svg' alt='camara-logo-dark' /> */}
 
           {/* Statement */}
-          <div className='statement'>
+          <div className="statement">
             <p>
               <span>&lt;h2&gt;</span>UI foundations for accessible, beautiful,
               and responsive interfaces.
@@ -178,42 +178,40 @@ const HomePage = () => {
           </div>
 
           {/* Bio */}
-          <div className='bio'>
+          <div className="bio">
             <p>
-              <span className='ptag'>&lt;p&gt;</span>Camara is a design system
+              <span className="ptag">&lt;p&gt;</span>Camara is a design system
               with
-              <span className='strongTag'>&lt;strong&gt;</span>
+              <span className="strongTag">&lt;strong&gt;</span>
               <strong>reusable UI component library</strong>
-              <span className='strongTag'>&lt;/strong&gt;</span>and APIs for the
-              best <span className='strongTag'>&lt;strong&gt;</span>
+              <span className="strongTag">&lt;/strong&gt;</span>and APIs for the
+              best <span className="strongTag">&lt;strong&gt;</span>
               <strong>default user and developer experience.</strong>
-              <span className='strongTag'>&lt;/strong&gt;</span>. This means
-              that you can <span className='strongTag'>&lt;strong&gt;</span>
+              <span className="strongTag">&lt;/strong&gt;</span>. This means
+              that you can <span className="strongTag">&lt;strong&gt;</span>
               <strong>spend less time</strong>
-              <span className='strongTag'>&lt;/strong&gt;</span> thinking about
+              <span className="strongTag">&lt;/strong&gt;</span> thinking about
               the nuances of user interface elements and constructing the
               product, but rather spend more time and{' '}
-              <span className='strongTag'>&lt;strong&gt;</span>
+              <span className="strongTag">&lt;strong&gt;</span>
               <strong>focus on what the product actually is</strong>
-              <span className='strongTag'>&lt;/strong&gt;</span>and the problems
+              <span className="strongTag">&lt;/strong&gt;</span>and the problems
               it is trying to solve.
-              <span className='ptag'>&lt;/p&gt;</span>
+              <span className="ptag">&lt;/p&gt;</span>
             </p>
           </div>
 
           {/* Call to Actions */}
-          <div className='cta'>
-            <Link to='/docs/'>
-              <Button pill backgroundColor='#75dd66' color='#000000'>
-                Learn More (Docs)
-              </Button>
+          <div className="cta">
+            <Link to="/docs/">
+              <Button pill>Learn More (Docs)</Button>
             </Link>
             <a
-              href='https://github.com/Camaradotspace'
-              target='_blank'
-              rel='noopener noreferrer'
+              href="https://github.com/Camaradotspace"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              <Button pill backgroundColor='#708090' color='#fff'>
+              <Button pill variant="secondary">
                 GitHub
               </Button>
             </a>
